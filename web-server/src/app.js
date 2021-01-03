4e464dd820589cb09d3ca84b005d3b398c89b8ef
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-04 04:17:30 +0530

    adding web server

---
+++ web-server/src/app.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const path= require('path')
+
+
+const app=express()
+const publicDirectoryPath= path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+// app.get('',(req,res)=>{
+//     res.send('<h1>Weather</h1>')
+// })
+// app.get('/help',(req,res)=>{
+//     res.send({
+//         name: 'Andrew',
+//         age: 27
+//     })
+// })
+// app.get('/about',(req,res)=>{
+//     res.send('About page')
+//})
+app.get('/weather',(req,res)=>{
+    res.send('Weather')
+})
+
+app.listen(3000,()=>{
+    console.log('Server is up on port 3000')
+})

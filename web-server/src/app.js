e727d20f75c21cde9db7917b3f0f415880f5c10f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-04 05:08:15 +0530

    setting hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,19 +4,22 @@ const path= require('path')
 
 const app=express()
 const publicDirectoryPath= path.join(__dirname, '../public')
+
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 app.get('',(req,res)=>{
     res.send('<h1>Weather</h1>')
 })
-app.get('/help',(req,res)=>{
-    res.send({
-        name: 'Andrew',
-        age: 27
-    })
-})
-app.get('/about',(req,res)=>{
-    res.send('About page')
-})
+// app.get('/help',(req,res)=>{
+//     res.send({
+//         name: 'Andrew',
+//         age: 27
+//     })
+// })
+// app.get('/about',(req,res)=>{
+//     res.send('About page')
+// })
 app.get('/weather',(req,res)=>{
     res.send('Weather')
 })

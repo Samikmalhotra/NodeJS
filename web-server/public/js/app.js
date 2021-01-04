892b35b5992e944c5f8eb165326fde2485ca2a83
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-04 15:18:20 +0530

    updated app.js

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,14 +5,23 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
         console.log(data)
     })
 })
-fetch('http://localhost:3000/weather?address=1!oston').then((response)=>{
+
+const weatherForm = document.querySelector('form')
+const search=document.querySelector('input')
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault(); 
+    const location=search.value
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if (data.error){
             console.log(data.error)
         }
         else{
             console.log(data.location)
-            console.log(data.forecast)
+            console.log(data.forecastData)
         }
+    })
 })
+
+    
 })

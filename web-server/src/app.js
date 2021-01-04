1ccb435afc4e7ba160afff4bdfed944728860407
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-04 05:30:19 +0530

    added hbs files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,36 +1,39 @@
-const express= require('express')
-const path= require('path')
+const path = require('path')
+const express = require('express')
 
-
-const app=express()
-const publicDirectoryPath= path.join(__dirname, '../public')
-
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req,res)=>{
-    res.render("index",{
-        title: 'Weather App',
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
         name: 'Sam'
     })
 })
-// app.get('',(req,res)=>{ 
-//     res.send('<h1>Weather</h1>')
-// })
-// app.get('/help',(req,res)=>{
-//     res.send({
-//         name: 'Andrew',
-//         age: 27
-//     })
-// })
-// app.get('/about',(req,res)=>{
-//     res.send('About page')
-// })
-app.get('/weather',(req,res)=>{
-    res.send('Weather')
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sam'
+    })
 })
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })

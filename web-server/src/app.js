bf4144e822e2617ed53716b1ac1d8156bae7b63d
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-04 05:50:34 +0530

    initialising partitions and views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,8 @@ const app = express()
 
 // Define paths for Express configs
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath= path.join(__dirname, '../templates')
+const viewsPath= path.join(__dirname, '../templates/views')
+const partialsPath= path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')

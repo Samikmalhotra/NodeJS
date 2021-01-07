e691a785617276db4fdc22365a063bd32de36b2f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-07 11:01:32 +0530

    Create api.js

---
+++ web-server/src/utils/api.js
@@ -0,0 +1,19 @@
+const request = require("request")
+const weather=(address,callback)=>{
+  const url='http://api.weatherapi.com/v1/current.json?key=00532184782c4fa190a40053210701&q='+encodeURIComponent(address)
+  request({url:url, json:true},(error, response, body)=>{
+    if(error){
+      callback("Unable to connect to location services",undefined)
+    }
+    else if(body.length === 0){
+      callback("Unable to find location", undefined)
+    }
+    else{
+      callback(undefined,{
+        location: body.location.name,
+        time: body.location.localtime,
+      })
+    } 
+  })
+}
+module.exports=weather

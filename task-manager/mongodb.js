20e7934c7df2384c376592c799058bcae68bcb76
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-12 17:35:25 +0530

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,4 +6,9 @@ const MongoClient=mongodb.MongoClient
 const connectionURL= 'mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
 
-MongoClient.connect(connectionURL,{useNewUrlParser:true})
+MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
+    if(error){
+        return console.log('Unable to connect to database!')
+    }
+    console.log('Connected correctly!')
+})

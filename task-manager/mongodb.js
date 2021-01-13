5bb7fb40868d6ab13a1acf88ca3aa85fb5fcc98c
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-13 15:05:11 +0530

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -4,6 +4,8 @@
 // const MongoClient=mongodb.MongoClient
 // const ObjectID=mongodb.ObjectID
 
+const {MongoClient, ObjectID}=require('mongodb')
+
 const connectionURL= 'mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
 

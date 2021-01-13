d9c2b64b35b9121197624f54815cd712c7035aab
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-13 15:06:52 +0530

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,6 +9,9 @@ const {MongoClient, ObjectID}=require('mongodb')
 const connectionURL= 'mongodb://127.0.0.1:27017'
 const databaseName='task-manager'
 
+const id = new ObjectID()
+console.log(id)
+
 MongoClient.connect(connectionURL,{useUnifiedTopology:true},(error,client)=>{
     if(error){
         return console.log('Unable to connect to database!')

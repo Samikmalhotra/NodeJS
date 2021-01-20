ac05f1c07156b39c282f61c93f5510337f039da5
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-21 02:36:34 +0530

    Update mongodb.js

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -109,17 +109,17 @@ MongoClient.connect(connectionURL,{useUnifiedTopology:true},(error,client)=>{
             //    }).catch((error) => {
             //        console.log(error)
             //    })
-            db.collection('tasks').updateMany({
-                completed: false
-            }, {
-                $set: {
-                    completed: true
-                }
-            }).then((result) => {
-                console.log(result.modifiedCount)
-            }).catch((error) => {
-                console.log(error)
-            })
+            // db.collection('tasks').updateMany({
+            //     completed: false
+            // }, {
+            //     $set: {
+            //         completed: true
+            //     }
+            // }).then((result) => {
+            //     console.log(result.modifiedCount)
+            // }).catch((error) => {
+            //     console.log(error)
+            // })
            
                 db.collection('users').deleteMany({
                     age: 27

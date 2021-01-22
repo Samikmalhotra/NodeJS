1a3a7cc48ec4c4793cbc98d41c11034d22424dbd
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-22 14:47:58 +0530

    Update mongoose.js

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,3 +4,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/tak-manager-api',{
     useNewUrlParser: true,
     useCreateIndex: true
 })
+
+const User = mongoose.model('User',{
+    name:{
+        type: String
+    },
+    age:{
+        type: Number
+    }
+})

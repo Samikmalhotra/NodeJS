d81f408210a9b69f9b3140927427b4278fd63a68
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-22 14:46:31 +0530

    Update mongoose.js

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,3 +1,6 @@
 const mongoose= require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017',)
+mongoose.connect('mongodb://127.0.0.1:27017/tak-manager-api',{
+    useNewUrlParser: true,
+    useCreateIndex: true
+})

54e75077d7afceb1901f40701f62101234afa4a7
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-22 17:20:42 +0530

    nodemon update

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,6 +3,10 @@ const express=require('express')
 const app= express()
 const port= process.env.PORT || 3000        
 
+app.post('/user',(req,res)=>{
+    res.send('testing!')
+})
+
 app.listen(port, ()=>{
     console.log('Server is up on port'+ port)
-})
+}) 

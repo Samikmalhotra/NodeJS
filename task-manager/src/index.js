49f7610caceea7495fa201ce86ea030ec0682fee
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-24 18:56:47 +0530

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,21 +1,16 @@
 const express=require('express')
-
+ 
 const app= express()
 const port= process.env.PORT || 3000        
 
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    console.log(req.body)
+    res.send('testing')
 })
 
 
 app.listen(port, ()=>{
-    console.log('Server is up on port'+ port)
+    console.log('Server is up on port '+ port)
 }) 

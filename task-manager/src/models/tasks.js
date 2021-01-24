a60a5b8d8585e40983e8966349b4fe6f012e3aa0
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-24 19:53:08 +0530

    Create tasks.js

---
+++ task-manager/src/models/tasks.js
@@ -0,0 +1,15 @@
+const mongoose= require('mongoose')
+
+const Task = mongoose.model('Task',{
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed:{
+        type: Boolean,
+        required: false,
+        trim: true,
+        default: false
+    }
+})

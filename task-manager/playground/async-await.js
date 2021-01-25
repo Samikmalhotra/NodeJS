652ac3c5891a5707df90b24619d0e754c8b4771a
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-25 22:05:50 +0530

    Update async-await.js

--- task-manager/playground/async-await.js
+++ task-manager/playground/async-await.js
@@ -1,9 +1,17 @@
-const doWork=async()=>{
-    throw new Error('Something went wrong')
-    return 'Sam'
+
+const add=(a,b)=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(a+b)
+        },2000)
+    })
 }
-
-
+const doWork=async()=>{
+    const sum = await add(1,99)
+    const sum2 = await add(sum, 50)
+    const sum3 = await add(sum2, 3)
+    return sum3
+}
 
 doWork().then((result)=>{
     console.log('result',result)

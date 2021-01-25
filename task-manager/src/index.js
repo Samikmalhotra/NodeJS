91e8f3f8946e6ca81401e7d3e32ebc5b9feb7193
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-25 19:31:47 +0530

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -26,7 +26,19 @@ app.get('/users',(req,res)=>{
     })
 })
 
-app.get('/users/:id')
+app.get('/users/:id',(req,res)=>{
+    const _id=req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+
+})
  
 app.post('/tasks', (req,res)=>{
     const task = new Task(req.body)

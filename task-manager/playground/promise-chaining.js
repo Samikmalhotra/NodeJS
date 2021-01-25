c80add95783b5a371bd45b84f6a3db418e1570da
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-25 22:16:17 +0530

    Update promise-chaining.js

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require('../src/models/users')
 
 //600aa1a0ccb793040466125f
 
-User.findByIdAndUpdate('600aa1a0ccb793040466125f',{ age: 1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})
-}).then((result)=>{
-    console.log(result)
+// User.findByIdAndUpdate('600aa1a0ccb793040466125f',{ age: 1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async(id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age: age})
+    const count = await User.countDocuments({age: age})
+    return count
+}
+
+updateAgeAndCount('600aad210462f01ff4516000', 2).then((count)=>{
+    console.log(count)
 }).catch((e)=>{
     console.log(e)
 })

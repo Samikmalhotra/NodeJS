5f8c232ec9552e772c718665bc9825efa1e521e9
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-26 00:18:43 +0530

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -123,26 +123,24 @@ app.get('/tasks/:id',async(req,res)=>{
     // })
 })
 
-app.patch('/users/:id',async(req,res)=>{
-    const _id = req.params.id
+app.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
-    const allowedUpdates = ["name", "email", "password", "age"]
-    const isValidOperation = updates.every(()=>{
-        return allowedUpdates.includes(updates)
-    })
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if(!isValidOperation){
-        return res.status(400).send({error: "Invalid updates!"})
-    } 
-
-    try{
-        const user = await User.findByIdAndUpdate(_id, req.body, {new:true, runValidators: true})
-        if(!user){
-            res.status(404).send()
-        }
-        res.send(user)
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
     }
-    catch(e){
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
         res.status(400).send(e)
     }
 })

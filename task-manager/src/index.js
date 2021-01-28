b76c02878e53c7859476edf505cb2ff7a94151c0
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-28 21:00:56 +0530

    created router for users

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,67 +9,6 @@ const port= process.env.PORT || 3000
 
 app.use(express.json())
 app.use(userRouter)
-
-app.post('/users', async(req, res) => {
-    const user = new User(req.body)
-
-    // user.save().then(()=>{
-    //     res.status(201).send(user)
-    // }).catch((error)=>{
-    //     res.status(400).send(error)
-    // })
-
-    try{
-        await user.save()
-        res.status(201).send(user)
-    }
-    catch(e){
-        res.status(400).send(e)
-    }
-    
-
-})
-
-app.get('/users',async(req,res)=>{
-    try{
-        const users = await User.find({})
-        res.send(users)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-    
-    // User.find({}).then((users)=>{
-    //     res.send(users)
-    // }).catch((error)=>{
-    //     res.status(500).send()
-    // })
-})
-
-app.get('/users/:id',async(req,res)=>{
-    const _id = req.params.id
-
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }
-    catch(e){
-        res.status(500).send()
-    }
-
-    // User.findById(_id).then((user)=>{
-    //     if(!user){
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e)=>{
-    //     res.status(500).send()
-    // })
-
-})
  
 app.post('/tasks', async(req,res)=>{
     const task = new Task(req.body)
@@ -125,27 +64,7 @@ app.get('/tasks/:id',async(req,res)=>{
     // })
 })
 
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
 
 app.patch('/tasks/:id', async(req,res)=>{
     const updates = Object.keys(req.body)

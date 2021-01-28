9f223e0b3790ed7db58e4a47c7ca7e8b015a5662
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-28 21:09:14 +0530

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,8 +11,6 @@ const port= process.env.PORT || 3000
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
- 
-
 
 app.listen(port, ()=>{
     console.log('Server is up on port '+ port)

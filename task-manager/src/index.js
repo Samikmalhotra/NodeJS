56b4de1729ab50c9fce95d3536e2bee6840bb4c2
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-01-28 20:31:26 +0530

    Update index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -169,6 +169,20 @@ app.patch('/tasks/:id', async(req,res)=>{
     }
 })
 
+app.delete('/users/:id', async(req,res)=>{
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            res.status(404).send()
+        }
+
+        res.send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, ()=>{
     console.log('Server is up on port '+ port)
 }) 

ab81cb17bdcf22369b8dcd8c6c07ca6deff3911d
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-02-03 03:45:20 +0530

    creating middleware (auth)

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -16,9 +16,9 @@ const port= process.env.PORT || 3000
 //     }
 // })
 
-app.use((req,res,next)=>{
-    res.status(503).send('Site is under maintenance')
-})
+// app.use((req,res,next)=>{
+//     res.status(503).send('Site is under maintenance')
+// })
 
 app.use(express.json())
 app.use(userRouter)

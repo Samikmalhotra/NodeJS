38935a4992979b8b42a7ab7015040b4079e407e2
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-02-05 00:10:19 +0530

    using toJSON

--- task-manager/src/models/users.js
+++ task-manager/src/models/users.js
@@ -48,13 +48,13 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
-userSchema.methods.getPublicProfile = function(){
+userSchema.methods.toJSON = function(){
     const user = this
     const userObject = user.toObject()
 
     delete userObject.password
     delete userObject.tokens
-    
+
     return userObject
 }
 

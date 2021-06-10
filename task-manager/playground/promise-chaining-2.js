5be6a731e148c1a5dad4f341a9a14b18faeb6582
Samik Malhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-06-11 00:43:06 +0530

    Update promise-chaining-2.js

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,14 +1,7 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/tasks')
 
-// Task.findByIdAndDelete('600d85bcd18793344c790c1d').then((task)=>{
-//     console.log(task)
-//     return Task.countDocuments({completed: false})
-// }).then((result)=>{
-//     console.log(result)
-// }).catch((e)=>{
-//     console.log(e)
-// })
+
 
 const deleteTaskAndCount = async(id, completed) => {
     await Task.findByIdAndDelete(id)
